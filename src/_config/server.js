23e717d19aa0ae11a525f35d14c5ff155304ee90
RenanMarcosAndreolla <renan.andreolla@compasso.com.br>
2021-03-08 18:48:30 -0300

    Projeto Integrador 3

---
+++ src/_config/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const nunjucks = require('nunjucks');
+const restify = require('restify');
+const mongoose = require('mongoose');
+const md5 = require('md5');
+
+module.exports = () => {
+    var port = process.env.port || 3000;
+
+    let env = nunjucks.configure('views', {
+        autoescape: true,
+        express: app
+    });
+    
+    app.set('engine', env);
+    
+    require('useful-nunjucks-filters')(env);
+    
+    app.listen(port, () => {
+        console.log('[LISTEN ON PORT ' + port + ']');
+    });
+    
+    //NUNJUCKS
+    app.use(bodyParser.json());       // to support JSON-encoded bodies
+    app.use(bodyParser.urlencoded({   // to support URL-encoded bodies
+    extended: true
+    }));
+    app.use(express.static('public'));
+    //
+    
+    //PÁGINAS
+    app.get('/', (req, res) => {
+        res.render('index.html');
+    });
+    
+    app.get('/register', (req, res) => {
+        res.render('register.html');
+    });  
+    //
+}
