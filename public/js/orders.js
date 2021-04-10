0653c444b93996c080ed0e9943402bc49601b76e
MarlonAngonese <marlonmaster2020@gmail.com>
2021-04-10 00:12:45 -0300

    Fix structure and add orders register

--- public/js/orders.js
+++ public/js/orders.js
@@ -1,28 +1,13 @@
 console.log("orders.js loaded");
 
-var orderNumber = $("#order-number");
-var orderHour = $("#order-hour");
 var clientName = $("#client-name");
 var serviceType = $("#service-type");
 var machineType = $("#machine-type");
 var orderDescription = $("#order-description");
 
-orderHour.mask("99:99");
 
 function sendData() {
     var validateInput = /[@!#$%^&*()='+_"?°~`<>{}\\]/;
-
-    if(orderNumber.val() == "") {
-        orderNumber.addClass("error-input");
-        toastr["error"]("Número do pedido inválido");
-        return;
-    }
-
-    if(orderHour.val() == "") {
-        orderHour.addClass("error-input");
-        toastr["error"]("Hora do pedido inválido");
-        return;
-    }
 
     if(clientName.val() == "" || validateInput.test(clientName.val()) == true) {
         clientName.addClass("error-input");
@@ -47,39 +32,15 @@ function sendData() {
         toastr["error"]("Descrição do serviço inválida");
         return;
     }
-
-    else {
-        var data = {
-            orderNumber: orderNumber.val(),
-            orderHour: orderHour.val(),
-            clientName: clientName.val(),
-            serviceType: serviceType.val(),
-            machineType: machineType.val(),
-            orderDescription: orderDescription.val()
-        }
-
-        console.log("data: ", data);
-        
-        clearInput();
-        
-        toastr["success"]("Pedido cadastrado com sucesso");
-    }
 }
 
 function removeInputError() {
-    orderNumber.removeClass("error-input");
-    orderHour.removeClass("error-input");
     clientName.removeClass("error-input");
     serviceType.removeClass("error-input");
     machineType.removeClass("error-input");
     orderDescription.removeClass("error-input");
 }
 
-function clearInput() {
-    orderNumber.val('');
-    orderHour.val('');
-    clientName.val('');
-    serviceType.val('');
-    machineType.val('');
-    orderDescription.val('');
+function success() {
+    toastr["success"]("Pedido cadastrado com sucesso");
 }
---
+++ src/models/Order.js
@@ -0,0 +1,19 @@
+const db = require('../_config/database');
+
+const Order = db.sequelize.define('orders', {
+    client_id: {
+        type: db.Sequelize.INTEGER
+    },
+    service_type: {
+        type: db.Sequelize.STRING
+    },
+    machine_type: {
+        type: db.Sequelize.STRING
+    },
+    service_description: {
+        type: db.Sequelize.TEXT
+    }
+});
+//Order.sync({force: true});
+
+module.exports = Order;
